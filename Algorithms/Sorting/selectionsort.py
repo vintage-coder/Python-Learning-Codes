3eb8e0058e5b3fda3796c56c8aa0e0642ec7eef5
vintage-coder <gowthamanvai@gmail.com>
2021-09-10 12:56:30 +0530

    selection sort

--- Algorithms/Sorting/selectionsort.py
+++ Algorithms/Sorting/selectionsort.py
@@ -1,2 +1,28 @@
 #This is the selection sort Algorithms
 
+def ssort(aList):
+    for i in range(len(aList)):
+        least=i
+        for j in range(i+1,len(aList)):
+            if aList[j]<aList[least]:
+                least=j
+        swap(aList,least,i)
+
+
+def swap(A,x,y):
+    temp=A[x]
+    A[x]=A[y]
+    A[y]=temp
+
+
+a=[]
+
+n=int(input('Enter the upper limit:'))
+
+for i in range(n):
+    a.append(int(input()))
+
+
+ssort(a)
+
+print('After sorting:',a)
