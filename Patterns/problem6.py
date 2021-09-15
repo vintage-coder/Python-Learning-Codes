e8d717e6175b4f1cbfafd3718abd4609e5b96c5c
vintage-coder <gowthamanvai@gmail.com>
2021-09-15 18:51:13 +0530

    pattern 6 updated..

---
+++ Patterns/problem6.py
@@ -0,0 +1,11 @@
+n=int(input('Enter the a number:'))
+
+for i in range(n):
+    for j in range(i):
+        print('*  ',end=" ")
+    print('')
+
+for i in range(n,0,-1):
+    for j in range(i):
+        print('* ',end=" ")
+    print('')
